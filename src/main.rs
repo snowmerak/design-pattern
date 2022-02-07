d55099c71a76aaab88301c9371772673f903f645
snowmerak <snowmerak@outlook.com>
2022-02-08 05:02:49 +0900

    update

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,23 @@
 pub mod factory;
+pub mod abstract_factory;
+pub mod builder;
+pub mod prototype;
+pub mod singletone;
+pub mod adapter;
+pub mod bridge;
+pub mod composite;
+pub mod decorator;
+pub mod flyweight;
+pub mod proxy;
+pub mod chain_of_responsibility;
+pub mod command;
+pub mod iterator;
+pub mod mediator;
+pub mod memento;
+pub mod observer;
+pub mod state;
+pub mod strategy;
+pub mod visitor;
 
 fn main() {
     
